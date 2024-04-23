d997222f12d23834a3da27219cb5e4bed5e83e56
RyoheiSuga <r.suga.24.4.1353@4nonome.com>
2024-04-23 13:32:53 +0900

    関数答え2

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -5,11 +5,20 @@ import math
 # >>> 0
 # -----------
 
-a = 0
-b = math.pi/2
-N = 100
-h = (b-a)/N
-S = 0
-for k in range(1,N+1):
-    S += h*(sin(a+(k-1)*h)+sin(a+k*h))/2
-print(S)
+# TODO
+def a(x):
+  return sin(x)
+def b(x):
+  return 4/(1+x**2)
+def c(x):
+  return math.pi**(1/2)*math.exp(-x**2)
+def f(d,a=0,b=1,n=100):
+  h = (b-a)/n
+  S = 0
+  for k in range(1,n+1):
+    S += h*(d(a+(k-1)*h)+d(a+k*h))/2
+  return S
+
+  print("(1)",f(a,0,math.pi/2,50))
+  print("(2)",f(b,0,1,100))
+  print("(3)",f(c,-100,100,1000))
