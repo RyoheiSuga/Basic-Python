681d5610411c6009483de781954337e30ab4307a
RyoheiSuga <r.suga.24.4.1353@4nonome.com>
2024-04-24 16:52:27 +0900

    関数答え4

--- euclid.py
+++ euclid.py
@@ -6,4 +6,18 @@ def euclid(a,b):
   while b!=0:
     a, b = b, a%b
   return a
+def so(a,b):
+  if euclid(a,b)==1:
+    return True
+  else:
+    return False
+
+import random
+s = 0
+for i in range(100000):
+  a = random.randint(1,10000)
+  b = random.randint(1,10000)
+  if so(a,b)==True:
+    s += 1
+print(s/100000)
   
