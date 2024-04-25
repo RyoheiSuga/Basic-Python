9fe6fb1c3ba7c904e8efb19066d27b4878205a31
RyoheiSuga <r.suga.24.4.1353@4nonome.com>
2024-04-25 22:19:00 +0900

    関数直し34

--- euclid.py
+++ euclid.py
@@ -1,23 +1,20 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
+
 
 # TODO
 def euclid(a,b):
   while b!=0:
     a, b = b, a%b
   return a
-def so(a,b):
-  if euclid(a,b)==1:
-    return True
-  else:
-    return False
+def coprime(a,b):
+  return euclid(a,b)==1
+    
 
 import random
 s = 0
 for i in range(100000):
   a = random.randint(1,10000)
   b = random.randint(1,10000)
-  if so(a,b)==True:
+  if coprime(a,b):
     s += 1
 print(s/100000)
   
