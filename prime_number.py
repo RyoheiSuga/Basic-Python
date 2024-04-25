e486d03481086290df8efaac11f0e969368155b6
RyoheiSuga <r.suga.24.4.1353@4nonome.com>
2024-04-25 21:52:50 +0900

    関数直し１

--- prime_number.py
+++ prime_number.py
@@ -1,19 +1,17 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+
 
 # TODO
 def sosuu(n):
-  if type(n) != int:
+  if not isinstance(n,int):
     return False
   elif n <= 1:
     return False
   else:
-    A = 1
     for i in range(2, n):
-      if n % i == 0:
-        A = 0
-    if A == 1:
-      return True
-    else:
-      return False
+        if n % i == 0:
+            A = 0
+            return False
+        else:
+            return True
+        
  
